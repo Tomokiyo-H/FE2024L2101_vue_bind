4edef5978edfce0899e4ca862d330b959dd75f41
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2024-12-03 10:32:33 +0900

    6-1

--- 6-1/script/script.js
+++ 6-1/script/script.js
@@ -2,10 +2,15 @@ new Vue({
     el: '#app',
     data() {
       return {
-        
+        range:10,
+        red:0,
+        blue:0,
+        greem:0
       };
     },
     computed: {
-      
+      bindStyle(){
+        return 'width: ${this.range}px; heigth: ${this.renge}px; background: rgb(${this.red,},${this.green},${this.blue})';
+      }
     }
   });
